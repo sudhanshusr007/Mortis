3357037c1c96ac72e593dcd976314d4b84fc51ae
Sudhanshu Ranjan <sudhanshusr007@gmail.com>
2024-06-17 20:03:42 +0530

    added create and login

--- server/index.js
+++ server/index.js
@@ -5,13 +5,26 @@ const express = require("express");
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 const { authenticateToken } = require("./utilities");
+const User = require("./models/user.model");
 
 const port = process.env.PORT || 8000;
 const mongoUri = process.env.connectionString;
 const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET;
 
-
 const app = express();
+
+mongoose
+  .connect(process.env.connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB", err);
+    process.exit(1); // Exit the process if connection fails
+  });
 
 app.use(express.json());
 
@@ -31,4 +44,120 @@ app.get("/protected", authenticateToken, (req, res) => {
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
+});
+
+app.post("/create-account", async (req, res) => {
+  const { fullName, email, password } = req.body;
+
+  if (!fullName) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Full Name is required" });
+  }
+
+  if (!email) {
+    return res.status(400).json({ error: true, message: "Email is required" });
+  }
+
+  if (!password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password is required" });
+  }
+
+  try {
+    const isUser = await User.findOne({ email: email });
+
+    if (isUser) {
+      return res.json({
+        error: true,
+        message: "User already exists",
+      });
+    }
+
+    const user = new User({
+      fullName,
+      email,
+      password,
+    });
+
+    await user.save();
+
+    const accessToken = jwt.sign(
+      { user: user._id },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "36000m",
+      }
+    );
+
+    return res.json({
+      error: false,
+      user,
+      accessToken,
+      message: "Registration Successful",
+    });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res
+      .status(500)
+      .json({ error: true, message: "Failed to create user" });
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email) {
+    return res.status(400).json({ message: "Email is required" });
+  }
+
+  if (!password) {
+    return res.status(400).json({ message: "Password is required" });
+  }
+
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const user = { user: userInfo };
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: "36000m",
+    });
+    return res.json({
+      error: false,
+      message: "Login Successful",
+      email,
+      accessToken,
+    });
+  } else {
+    return res.status(400).json({
+      error: true,
+      message: "Invalid Credentials",
+    });
+  }
+});
+
+app.get("/get-user", authenticateToken, async (req, res) => {
+  const { user } = req.user;
+  const isUser = await User.findOne({
+    _id: user._id,
+  });
+
+  if (!isUser) {
+    return res.sendStatus(401);
+  }
+
+  return res.json({
+    user: {
+      fullName: isUser.fullName,
+      email: isUser.email,
+      _id: isUser._id,
+      createdOn: isUser.createdOn,
+    },
+    message: "",
+  });
 });
