c1cb91cdb9cd2736292035e4c7669caa1568fbdc
Shivam Singh <shivamsingh@Shivams-Mac.local>
2024-07-03 22:42:23 +0530

    Front end complete

--- client/src/components/UserCard.jsx
+++ client/src/components/UserCard.jsx
@@ -3,12 +3,21 @@ import React from 'react';
 
 const UserCard = ({ user }) => {
   return (
-    <div className="card bg-white shadow-md rounded-lg p-4">
-      <img src={user.avatar || 'https://via.placeholder.com/150'} alt={user.name} className="w-16 h-16 rounded-full mx-auto mb-4" />
-      <h2 className="text-xl font-semibold">{user.name}</h2>
-      <p>{user.email}</p>
-      {/* Add more fields as needed */}
+    <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5 p-6">
+    <div class="flex flex-col">
+        <h2 class="text-2xl font-bold text-center mb-4">Personal Information</h2>
+        <div class="space-y-2 flex flex-col justify-start">
+            <p><strong>First Name:</strong> Shivam</p>
+            <p><strong>Last Name:</strong> Singh</p>
+            <p><strong>Date of Birth:</strong> 26/12/2001</p>
+            <p><strong>Gender:</strong> Male</p>
+            <p><strong>Email</strong> singh.shvam26gmail.com</p>
+            <p><strong>Phone</strong> 99999999999</p>
+            <p><strong>Age</strong> 22</p>
+         
+        </div>
     </div>
+</div>
   );
 };
 
--- client/src/pages/DashBoard.jsx
+++ client/src/pages/DashBoard.jsx
@@ -4,7 +4,7 @@ import UserCard from '../components/UserCard'; // Adjust the import path as nece
 import { motion } from 'framer-motion';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
-import Query from '../components/Query';
+import { FaArrowRight } from 'react-icons/fa6';
 
 const Dashboard = () => {
   // Simulate fetching user data
@@ -12,7 +12,23 @@ const Dashboard = () => {
 
   return (
  <>
-Dash
+<Navbar />
+<div className='bg-[#42eb61] -z-10 absolute top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] 
+      sm:w-[68.75rem]'></div>
+      <div className='bg-[#c5ebd0] -z-10 absolute top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] 
+      sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+  <UserCard />
+  <br />
+  <br />
+  <br />
+  <h1 className='text-3xl text-center font-bold'>Book an <span className='text-green-800'>Appointment</span></h1>
+  <div className='flex justify-center p-4'>
+  <a href="/schedule" className="inline-flex items-center justify-center px-4 py-2 text-xs font-medium text-white bg-black rounded-lg hover:bg-gray-800 transition duration-150 ease-in-out">
+      <span>Schedule</span>
+      <FaArrowRight className="ml-2" />
+    </a>
+    </div>
+<Footer />
  </>
   );
 };
